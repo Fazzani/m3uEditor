2acd73e9cd28167bf19e885a6a265bf9095ac3cb
Fazzani <fazzani.heni@outlook.fr>
2020-06-08 23:35:23 +0200

    Fix debug app

--- fix-elctronsource-forge.sh
+++ fix-elctronsource-forge.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-cat > 'node_modules/.bin/electron-forge-vscode-nix'  <<- EOM
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-ARGS=$@
-ARGS=${ARGS// /\~ \~}
-
-node $DIR/../@electron-forge/cli/dist/electron-forge-start --vscode -- \~$ARGS\~
-EOM
+curl -s https://gist.githubusercontent.com/Fazzani/2c4bad108c47e3924dc964af1d1b770c/raw/27abd62520574cdf0fe1202aa4fd97a70cccf0e6/electron-forge-vscode-nix > node_modules/.bin/electron-forge-vscode-nix
